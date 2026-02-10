d12a65605bd9dda3be58fa9da4b9b34dcd8f6bd5
SavvinaFil <filtsavv@gmail.com>
2026-02-10 16:35:58 +0200

    Update __init__.py

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -1,10 +1,9 @@
 from .tabular.tree_based import run_tabular_analysis
-
-# from .timeseries.lstm_based import run_timeseries_analysis
+from .timeseries.lstm_based import run_timeseries_analysis
 
 ANALYSIS_ROUTER = {
     "tabular": run_tabular_analysis,
-    # "timeseries": run_timeseries_analysis
+    "timeseries": run_timeseries_analysis
 }
 
 
